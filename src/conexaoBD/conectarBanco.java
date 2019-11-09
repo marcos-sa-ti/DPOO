9accf83adb232cd1ad6342adea410a9064bf820d
Marcos Sá <>
2019-11-08 21:33:59 -0300

    Inserção de novos pacotes Inicialiação pela tela inicial Adicionado metodo para conectar com o banco de dados

--- src/conexaoBD/conectarBanco.java
+++ src/conexaoBD/conectarBanco.java
@@ -1,9 +1,38 @@
 package conexaoBD;
 
-/**
- *
- * @author marcos.sbrito2
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class conectarBanco {
     
+    public  static Connection getConexao() throws SQLException, ClassNotFoundException {
+        
+        try {
+            Connection connection = null;
+            String driverName = "com.mysql.jdbc.Driver";
+            Class.forName(driverName);
+            // NOME DO BANCO DE DADOS
+            String nomeBD = "";
+            // COLOCAR A PORTA
+            String url = "jdbc:mysql://localhost:3306/" + nomeBD + "?zeroDateTimeBehavior=convertToNull";
+            // USUARIO
+            String username = "root";
+            // SENHA
+            String password = "123456";
+            connection = DriverManager.getConnection(url, username, password);
+            return connection;
+
+        } catch (SQLException e) {
+            System.out.println("Erro na conexão do banco de dados: " + e);
+            return null;
+        }
+    }
+
+    public void restartConnection() throws SQLException, ClassNotFoundException {
+        getConexao().close();
+        getConexao();
+    }
+    
+
 }
